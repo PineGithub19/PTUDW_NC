338759547ed00881d27118fdacc43dce4bc57f28
PineGithub19 <114576524+PineGithub19@users.noreply.github.com>
2025-10-13 08:36:09 +0700

    Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActorsModule } from './actors/actors.module';
 import { Actor } from './actors/entities/actor.entity';
 import { FilmsModule } from './films/films.module';
+import { Films } from './films/entities/films.entity';
 
 @Module({
   imports: [
@@ -13,9 +14,9 @@ import { FilmsModule } from './films/films.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'mysqltungthai19',
-      database: 'actors',
-      entities: [Actor],
+      password: 'root',
+      database: 'ptudwnc',
+      entities: [Actor, Films],
       synchronize: false,
       autoLoadEntities: true,
     }),
